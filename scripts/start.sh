964cd17be54e06b555f862bd108873850ead8a39
root <josep@datag.es>
2021-05-07 13:04:05 +0000

    Paths

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,7 +29,7 @@ end_port=$[$start_port+$n_servers]
 
 while [ $start_port -lt $end_port ]
 do
-/root/quantra/build/server/async_server $start_port &
+$QUANTRA_HOME/build/server/async_server $start_port &
 start_port=$[$start_port+1]
 done
 
