5084886d25920d44110c752f227b972bdc2bbfa6
root <joseprupi@gmail.com>
2021-08-16 21:36:51 +0000

    Certificate

--- jsonserver/server.cpp
+++ jsonserver/server.cpp
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 
     int port = atoi(argv[2]);
 
-    QuantraClient base_client(connection, false);
+    QuantraClient base_client(connection, true);
 
     CROW_ROUTE(app, "/")
         .name("hello")([]
