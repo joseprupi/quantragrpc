adbfa3d521aa89a38241382d98b6eca61fb3b517
root <joseprupi@gmail.com>
2022-07-21 15:04:57 +0000

    Cleaned

--- flatbuffers/CMakeFiles/fatbuffers.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fatbuffers"
-  "fbs/fbs.stamp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fatbuffers.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
